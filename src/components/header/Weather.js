81144a04e662f8b87d6bc5f99f051733171fdb38
Marta Kowalska <martaaa.kowalska@gmail.com>
2021-09-09 10:29:45 +0200

    [update] weather component to use new API

--- src/components/header/Weather.js
+++ src/components/header/Weather.js
@@ -2,24 +2,19 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 export default function Weather() {
-  const WEATHER_API_KEY = process.env.REACT_APP_API_KEY_WEATHER;
+  const city = 'Budapest';
+  const currentWeatherURL = `http://localhost:8080/weather/v1/city=${city}`;
 
-  const currentWeatherURL = `https://api.weatherbit.io/v2.0/current?city=Budapest&key=${WEATHER_API_KEY}`;
-  const iconURL = "https://www.weatherbit.io/static/img/icons/icon_code.png";
-
-  const [temperature, setTemperature] = useState([]);
+  const [temperature, setTemperature] = useState("");
   const [icon, setIcon] = useState("");
   const [weatherStatus, setWeatherStatus] = useState("");
 
-  const insertIconCode = (iconCode) => {
-    return iconURL.replace("icon_code", iconCode);
-  };
 
   useEffect(() => {
     axios.get(currentWeatherURL).then((response) => {
-      setTemperature(response.data.data[0].app_temp);
-      setIcon(insertIconCode(response.data.data[0].weather.icon));
-      setWeatherStatus(response.data.data[0].weather.description);
+      setTemperature(response.data.temperature);
+      setIcon(response.data.icon);
+      setWeatherStatus(response.data.description);
     });
   }, [currentWeatherURL]);
 
